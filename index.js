8c11606869f2fe911616a3d754f272e9abec01a7
Matthew Baty <mattbaty@outlook.com>
2023-04-08 15:35:55 -0400

    questions done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateLogo = require("./lib/generateLogo");
+const { check } = require("yargs");
 
 // number of characters check
 const checkLength = async (input) => {
@@ -12,3 +13,34 @@ const checkLength = async (input) => {
     }
 }
 
+// prompt questions
+const questions = inquirer.prompt([
+
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Please enter the text for your logo (3 Characters):'
+        validate: checkLength,
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Please enter the color you would like your text to be:'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Please select the shape you would like your logo to be:'
+        choices: ['Triangle', 'Circle', 'Square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Please enter the color you would like your logo to be:'
+    },    
+]) .then(response => writeToFile(response));
+
+//write file and catch errors
+function writeToFile(response) {
+    fs.writeFile("logo.svg", generateLogo(response), (err) => err ? console.log(err) : console.log("Generated logo.svg!"));
+}
